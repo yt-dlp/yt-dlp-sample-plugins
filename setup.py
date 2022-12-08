9b39873413ee6ab57b3027e02d6e887bd4ec7ac9
coletdjnz <coletdjnz@protonmail.com>
2022-12-08 14:15:04 +1300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name='ytdlp-sample-plugins',
-    packages=find_namespace_packages(include=[r'ytdlp_plugins.*'])
+    name='yt-dlp-sample-plugins',
+    packages=find_namespace_packages(include=[r'yt_dlp_plugins.*'])
 )
